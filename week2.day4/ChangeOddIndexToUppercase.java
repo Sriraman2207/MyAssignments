60b1eaef8c997a1afa4f802362c41671ee09531d
Sriraman2207 <109962304+Sriraman2207@users.noreply.github.com>
2022-08-02 17:05:05 +0530

    Add files via upload

---
+++ week2.day4/ChangeOddIndexToUppercase.java
@@ -0,0 +1,10 @@
+package week2.day4;
+
+public class ChangeOddIndexToUppercase {
+	public static void main(String[] args) {
+		String test="changeme";
+		String uppercase=test.toUpperCase();
+		System.out.println(uppercase);
+
+}
+}
---
+++ week2.day4/ReverseEvenNumbers.java
@@ -0,0 +1,26 @@
+package week2.day4;
+
+public class ReverseEvenNumbers {
+public static void main(String[] args) {
+		
+		String test = "I am a software tester";
+		String[] split = test.split(" ");
+		String convertedTest="";
+		for(int i=0;i<split.length;i++) {
+			if(i%2!=0) {
+				char[] charArray = split[i].toCharArray();
+				String rev="";
+				for(int j=charArray.length-1;j>=0;j--) {
+					rev=rev+charArray[j];
+	}
+				convertedTest=convertedTest+" "+rev;
+}
+			else {
+				convertedTest=convertedTest+" "+split[i];
+}
+	}
+		System.out.println(convertedTest);
+	}
+}
+
+
