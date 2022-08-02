db522f07cff5a15e7c400c3fe6db8c6a2a35bcd6
Sriraman2207 <109962304+Sriraman2207@users.noreply.github.com>
2022-08-02 17:02:21 +0530

    Add files via upload

---
+++ week.2day1/week2.day2/ArmstrongNumber.java
@@ -0,0 +1,23 @@
+package week2.day2;
+
+public class ArmstrongNumber {
+public static void main(String[] args) {
+		int number=153;
+		int r=0,sum=0;
+		int temp=number;
+		
+		while(temp>0) {
+	    r=temp%10;
+			
+		sum=sum+(r*r*r);
+		temp=temp/10;
+		}
+		if(sum==number) {
+		System.out.println("  Armstrong number:"+number);
+		}
+		else {
+		System.out.println(" not an Armstrong number:"+number);
+
+}
+	}
+}
---
+++ week.2day1/week2.day2/CheckIfTheNumberIsPositiveOrNegative.java
@@ -0,0 +1,14 @@
+package week2.day2;
+
+public class CheckIfTheNumberIsPositiveOrNegative {
+	public static void main(String[] args)   
+	{  
+	int num=35;  
+	if(num>0) {
+	System.out.println("Number is POSITIVE");
+	}
+	else {
+	System.out.println("Number is NEGATIVE");
+	}
+	}
+	}
